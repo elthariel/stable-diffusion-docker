b565c855fc5243f9d198c4fdfe2be9d97c0c552a
Julien 'Lta' BALLET <contact@lta.io>
2024-04-05 23:23:00 +0200

    automated s3 sync and various other small things

--- scripts/post_start.sh
+++ scripts/post_start.sh
@@ -1,26 +1,24 @@
 #! /bin/bash
 
 if [[ -n "$AWS_SECRET_ACCESS_KEY" ]]; then
-    conf="[sync]\ntype = s3\nprovider = AWS"
-    conf="$conf\naccess_key_id=$AWS_ACCESS_KEY_ID"
-    conf="$conf\nsecret_access_key=$AWS_SECRET_ACCESS_KEY"
-    conf="$conf\region=$AWS_DEFAULT_REGION"
     mkdir -p ~/.config/rclone
-    echo "$conf" > ~/.config/rclone/rclone.conf
+    conf_path="$HOME/.config/rclone/rclone.conf"
+
+    echo "[sync]" > "$conf_path"
+    echo 'type=s3' >> "$conf_path"
+    echo 'provider=AWS' >> "$conf_path"
+    echo "access_key_id=$AWS_ACCESS_KEY_ID" >> "$conf_path"
+    echo "secret_access_key=$AWS_SECRET_ACCESS_KEY" >> "$conf_path"
+    echo "region=$AWS_DEFAULT_REGION" >> "$conf_path"
 
     rclone copy -P --fast-list --transfers=4 \
-           "sync:$S3_SYNC_PATH" "/workspace/$HF_USERNAME"
-fi
-
-if [[ -d /workspace/elthariel/experiments ]]; then
-    echo "Running Lta's setup scripts..."
-    python /workspace/elthariel/experiments/helpers/system.py
-    python /workspace/elthariel/experiments/start.py
+           "sync:$S3_SYNC_PATH" "/workspace/$HF_USERNAME" \
+           | tee /tmp/rclone.log
 fi
 
 echo "Installing extensions..."
-cd /stable-diffusion-webui
-git clone --depth=1 https://github.com/Zyin055/Config-Presets.git\
+cd /workspace/stable-diffusion-webui
+git clone --depth=1 https://github.com/Zyin055/Config-Presets.git \
     extensions/Config-Presets
 
 
@@ -30,3 +28,9 @@ cfg_dir="$HOME/.jupyter/lab/user-settings/@jupyterlab/apputils-extension"
 cfg_file="$cfg_dir/themes.jupyterlab-settings"
 mkdir -p "$cfg_dir"
 echo "$cfg" > "$cfg_file"
+
+if [[ -d /workspace/elthariel/experiments ]]; then
+    echo "Running Lta's setup scripts..."
+    python /workspace/elthariel/experiments/helpers/system.py
+    python /workspace/elthariel/experiments/start.py
+fi
